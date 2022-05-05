e22da6526765b469ca9db08cc1b3dbdd2592765f
Lyn Finman <lynfinman@gmail.com>
2022-05-05 18:05:25 -0400

    changed background image and scripted mouse over logo

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+window.onload = function() {
+    /* 
+    This worked in the console but failed when added to the script, with error
+    that logoElement was null.  I think the script was being run before the ID
+    had been assigned to the img in the HTML file, so getElementById('logoImg') failed.
+    Could that be true?  Putting this inside the window.onload function solved the problem,
+    but is that the best answer?
+    */
+    const logoOnMouseOver = "./img/FinDevBubbles-logos_transparent.png";
+    const logoOnMouseOut = "./img/FinDev-logos_transparent.png";
+    const logoElement = document.getElementById('logoImg');
+    logoElement.addEventListener('mouseover', function (event) { 
+        logoElement.setAttribute('src', logoOnMouseOver); 
+    });
+
+    logoElement.addEventListener('mouseout', function(){
+        logoElement.setAttribute('src',logoOnMouseOut);
+        //alert('mouseout detected');
+    });
+}
